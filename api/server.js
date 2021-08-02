1d148fe247b8ba5f32a7101cdcae02d1aaf295e2
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-01 20:06:31 -0700

    index + server wired

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const recipesRouter = require('./recipes/recipe-router');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/recipes', recipesRouter);
+
+module.exports = server;
