8441051f5011b50ef1a75746bf785d71ad688670
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-01 21:09:16 -0700

    wip_migrations

--- data/migrations/20210802035421_initial-migration.js
+++ data/migrations/20210802035421_initial-migration.js
@@ -2,9 +2,12 @@
 exports.up = async function (knex) {
   await knex.schema.createTable('recipes', table => {
     table.increments();
+    table.string('recipe_name', 200).notNullable().unique();
   });
   await knex.schema.createTable('ingredients', table => {
-    table.increments();
+    table.increments('ingredient_id');
+    table.string('ingredient_name', 250).notNullable().unique();
+    table.string('ingredient_unit', 50);
   });
   await knex.schema.createTable('steps', table => {
     table.increments();
