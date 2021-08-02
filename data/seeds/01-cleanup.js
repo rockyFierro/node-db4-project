e1738553f3809a90b34c0f6a51af4010736eaf5c
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-01 21:42:52 -0700

    seed

--- data/seeds/01-cleanup.js
+++ data/seeds/01-cleanup.js
@@ -3,6 +3,6 @@ const {clean} = require('knex-cleaner');
 exports.seed = function (knex) {
   return clean(knex, {
     mode: 'truncate',
-    ignoreTable: ['knex_migrations', 'knex_migrations_lock'],
+    ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
   });
 };
